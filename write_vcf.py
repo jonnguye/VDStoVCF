04350c854cb17746c4937055e665c55334350969
jonnguye <144196218+jonnguye@users.noreply.github.com>
2025-05-15 17:40:56 -0700

    Create write_vcf.py

---
+++ write_vcf.py
@@ -0,0 +1,66 @@
+import hail as hl
+import argparse
+import os
+
+def write_vcf(inputs):
+    #LOAD TABLES AND FIND SUBSET
+    mt = hl.read_matrix_table(inputs['matrix_table'])
+    samples_table = hl.import_table(inputs['samples_list'], no_header=True)
+    samples_table = samples_table.rename({'f0': 's'})
+    samples_table = samples_table.key_by('s')
+    
+    mt = mt.filter_cols(hl.is_defined(samples_table[mt.s]))
+    print(f"Filtering to {mt.count_cols()} samples")
+
+    #SELECT WHICH CHROMOSOME TO FILTER BY
+
+    if inputs['chr'] is None or inputs['chr'].upper() == 'ALL':
+        print("NO CHR FILTER APPLIED")
+        print(f"CHR value provided: {inputs['chr']}")
+    else:
+        print(f"Filtering on {inputs['chr']}")
+        mt = mt.filter_rows( (mt.locus.contig == inputs['chr']) )
+    
+    hl.export_vcf(mt, f"{inputs['output_prefix']}.vcf.bgz")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--matrix_table", required=True)
+    parser.add_argument("--samples_list", required=True)
+    parser.add_argument("--chr", required=True)
+    parser.add_argument("--MinimumAC_inclusive", type=int, required=True)
+    parser.add_argument("--output_prefix", required=True)
+
+    args = parser.parse_args()
+
+    inputs = {
+        'matrix_table': args.matrix_table,
+        'samples_list': args.samples_list,
+        'chr': args.chr,
+        'MinimumAC_inclusive': args.MinimumAC_inclusive,
+        'output_prefix': args.output_prefix,
+    }
+
+    hl.init(
+        app_name='hail_job',
+        master='local[*]',
+        tmp_dir='gs://fc-secure-b8771cfd-5455-4292-a720-8533eb501a93/hail-tmp/',
+        spark_conf={
+            'spark.executor.instances': '4',
+            'spark.executor.cores': '8',
+            'spark.executor.memory': '25g',
+            'spark.driver.memory': '30g',
+            'spark.local.dir': '/cromwell_root',
+            'spark.sql.shuffle.partitions': '100',
+            'spark.default.parallelism': '100',
+            'spark.memory.fraction': '0.8',
+            'spark.memory.storageFraction': '0.2',
+        },
+        default_reference='GRCh38'
+    )
+    
+    print("Spark local directories:", os.getenv("SPARK_LOCAL_DIRS"), flush=True)
+    print("Disk usage:", flush=True)
+    os.system("df -h")
+    
+    write_vcf(inputs)
